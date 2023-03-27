25d9a244d2c96d6369bd2c68450c09660eb4e28f
Gonzalo <gonzalodonoso@valposystems.com>
2023-03-27 19:52:13 -0300

    Noveno Despliegue

--- Modulos/Principal/Funciones.js
+++ Modulos/Principal/Funciones.js
@@ -60,7 +60,7 @@ function obtenerPersonaje(){
             
             console.log(provisoriaRespuesta);
             
-            if (provisoriaRespuesta !== "Sin Personajes Registrados.") {
+            /*if (provisoriaRespuesta !== "Sin Personajes Registrados.") {
 
                 let respuesta = JSON.parse(provisoriaRespuesta);
                 
@@ -108,7 +108,7 @@ function obtenerPersonaje(){
                 let mensaje = ", puedes crear un nuevo personaje.";
                 mostrarSaludoInicial(mensaje);
                 $("#panelMensajeCarga").modal("hide");  
-            }
+            }*/
         }
     });         
 }
